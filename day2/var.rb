f0c7b65503d3d6691f1813b26a98ea5ca3b917bd
aglili <cecilselorm34@gmail.com>
2023-10-16 23:52:44 +0000

    day3

---
+++ day2/var.rb
@@ -0,0 +1,41 @@
+# Variables
+
+
+# age = 20
+
+# puts "My age is #{age}"
+
+
+
+# my_name = "John"
+
+# your_name = my_name
+
+
+# your_name.upcase
+
+# puts my_name
+
+
+sum = 0
+
+[1,2,3,4,5,6,7,8,9,10].each { |i| sum += i }
+
+puts sum
+
+
+total = 0
+
+[1,2,3,4,5,5,6,7,7,8,8].each do |i|
+  total += i
+end
+
+puts 
+
+a = 10
+
+3.times do |n|
+    a= 3
+end
+
+puts a
---
+++ day3/cl.rb
@@ -0,0 +1,31 @@
+
+
+if 100>= 2
+    puts '100 is greater than 2'
+else
+    puts '100 is not greater than 2'
+end
+
+
+number = 10
+
+
+puts 'number is greater than 2' if number > 2
+
+
+
+
+number1  = 10
+number2 = 10
+
+
+puts number1.equal?(number2)    
+
+
+# Spaceship operator
+
+puts  5<=> 10 #-1 means left side is smaller than right side
+
+puts 10 <=> 10 #0 means left side is equal to right side
+
+puts 10 <=> 5 #1 means left side is greater than right side
